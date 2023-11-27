2d74a9e0c965d5f0496983e7c254edf4158a3d94
Salliberg <37288752+Salliberg@users.noreply.github.com>
2023-11-27 13:25:06 -0500

    Add files via upload

---
+++ src/extract.py
@@ -0,0 +1,44 @@
+import wikipedia
+import numpy as np
+import pandas as pd
+
+def getwikidata(query):
+    try:
+        page=wikipedia.page(query)
+    except:
+        print(f'This query {query} not match any page.')
+        return
+    
+    url=page.url
+    q='_'.join(query.split(' '))
+    
+    data={'title':[],'contents':[],'summaries':[],'urls':[],'categories':[],'links':[]}
+    data['title'].append(q)
+    data['summaries'].append(page.summary)
+    data['categories'].append(','.join(page.categories))
+    data['links'].append(','.join(page.links))
+    data['urls'].append(page.url)
+    if url.split('/')[-1]==q:
+        ## query is the title of page in wiki
+        data['contents'].append(page.content)
+        
+    else:
+        data['contents'].append('')
+        print(len(page.links))
+        for link in page.links:
+            print(link)
+            try:
+                new=wikipedia.page(link)
+                data['title'].append(link)
+                data['contents'].append(new.content)
+                data['summaries'].append(new.summary)
+                data['categories'].append(','.join(new.categories))
+                data['links'].append(','.join(new.links))
+                data['urls'].append(new.url)
+            except:
+                print(f'This id {link} not match any page')
+                
+    return pd.DataFrame(data)
+
+    
+    
