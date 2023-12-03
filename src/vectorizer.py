c64d3741ca9fcc12d20575ac88596a25d9ca185c
shaan <skhoslajr@gmail.com>
2023-12-03 17:30:33 -0500

    changes

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer  # type: ignore
+from sklearn.decomposition import PCA  # type: ignore
 
 
 class Vectorizer:
@@ -16,3 +17,9 @@ class Vectorizer:
             show_progress_bar=True,
             convert_to_numpy=True,
         )
+
+    def dimensionality_reduction(
+        self, vectors: list[np.ndarray], components: int = 2
+    ) -> np.ndarray:
+        pca = PCA(n_components=components)
+        return pca.fit_transform(vectors)
